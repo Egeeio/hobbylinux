f3bdd44b1739efd7a4695155bd46983f6716c100
egee-irl <brian@egee.io>
2024-04-08 14:31:43 -0700

    Fill out helpers, use popen for chroot runner

--- src/lib/helpers.rb
+++ src/lib/helpers.rb
@@ -1,15 +1,26 @@
-def arch_chroot_runner
-    
+# frozen_string_literal: true
+
+require 'io'
+
+def arch_chroot_runner(cmd, chroot = '/mnt')
+  command = "arch-chroot #{chroot} /bin/bash -c '#{cmd}'"
+  IO.popen(command) do |io|
+    puts line.chomp while line == io.gets
+  end
 end
 
-def pacman_install
-    
+def pacman_install(pkgs, update = nil)
+  cmd = 'pacman --noconfirm -S'
+  "#{cmd}y" if update
+  "#{cmd} #{pkgs}"
 end
 
-def sed
-    
+def sed(original, replacement, file_uri)
+  "sed -i 's/#{original}/#{replacement}/' #{file_uri}"
 end
 
-def activate_service
-    
+def activate_service(srv, start = nil)
+  cmd = 'systemctl enable'
+  "#{cmd} --now" if start
+  "#{cmd} #{srv}"
 end
