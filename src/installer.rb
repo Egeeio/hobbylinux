5faccf9cdb54910d473811629027c02fa8fbb6f8
egee-irl <brian@egee.io>
2024-04-08 11:51:08 -0700

    Test partition functions

--- src/installer.rb
+++ src/installer.rb
@@ -1,20 +1,22 @@
 #!/usr/bin/env ruby
 require 'optparse'
+require_relative 'lib/partition'
 
 options = {}
 
 OptionParser.new do |opts|
     opts.banner = "Welcome to the Hobby Linux Installer! 🚀\nUsage: ./installer [options]"
-  
+
     opts.on("-i", "--install", "Install Hobby Linux") do |v|
-      puts "It's installed!"
+      drives = partition
+      puts drives
       exit
     end
-  
+
     opts.on("-d", "--debug", "Run in debug mode") do |v|
       options[:debug] = v
     end
-  
+
     opts.on("-h", "--help", "Show this help message") do
       puts opts
       exit
