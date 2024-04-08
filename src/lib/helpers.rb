5d38732c82dbfff63c242ffcbcb82721ce75428c
egee-irl <brian@egee.io>
2024-04-08 16:03:12 -0700

    refactor stream cmd

--- src/lib/helpers.rb
+++ src/lib/helpers.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
-def arch_chroot_runner(cmd, chroot = '/mnt')
-  command = "arch-chroot #{chroot} /bin/bash -c '#{cmd}'"
-  IO.popen(command) do |io|
+
+def stream_cmd(cmd)
+  IO.popen(cmd) do |io|
     while line = io.gets
       puts line.chomp
     end
   end
+end
+
+def arch_chroot_runner(cmd, chroot = '/mnt')
+  chroot_cmd = "arch-chroot #{chroot} /bin/bash -c '#{cmd}'"
+  stream_cmd(chroot_cmd)
 end
 
 def pacman_install(pkgs, update = nil)
