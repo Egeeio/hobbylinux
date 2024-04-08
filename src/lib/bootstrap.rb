5860e45983ac48a6c0f2e4280040f8af9871eed8
egee-irl <brian@egee.io>
2024-04-08 16:05:47 -0700

    refactor bootstrap to use stream cmd

--- src/lib/bootstrap.rb
+++ src/lib/bootstrap.rb
@@ -1,18 +1,17 @@
 require_relative 'helpers'
 
 def bootstrap()
-  puts `pacstrap -K /mnt base linux-lts btrfs-progs`
-  arch_chroot_runner("echo hobbylinux > /etc/hostname")
+  stream_cmd("pacstrap -K /mnt base linux-lts btrfs-progs archlinux-keyring syslinux nano sudo fish neofetch")
   arch_chroot_runner("pacman-key --init && pacman-key --populate")
-  arch_chroot_runner(pacman_install('archlinux-keyring syslinux nano sudo fish neofetch', true))
   arch_chroot_runner("syslinux-install_update -i -m -a")
-  arch_chroot_runner("sed -i 's/sda3/sda1/' /boot/syslinux/syslinux.cfg")
-  arch_chroot_runner("sed -i 's/# Misc options/ILoveCandy/' /etc/pacman.conf")
-  arch_chroot_runner("sed -i 's/vmlinuz-linux/vmlinuz-linux-lts/' /boot/syslinux/syslinux.cfg")
-  arch_chroot_runner("sed -i 's/initramfs-linux/initramfs-linux-lts/' /boot/syslinux/syslinux.cfg")
-  arch_chroot_runner("sed -i 's/Arch Linux/Hobby Linux/' /boot/syslinux/syslinux.cfg")
-  arch_chroot_runner("sed -i 's/Arch Linux/Hobby Linux/' /etc/os-release")
-  arch_chroot_runner("sed -i 's/Arch Linux/Hobby Linux/' /usr/lib/os-release")
-  arch_chroot_runner("cp /etc/systemd/network/* /mnt/etc/systemd/network/")
+  stream_cmd(sed('sda3', 'sda1', '/mnt/boot/syslinux/syslinux.cfg'))
+  stream_cmd(sed('vmlinuz-linux', 'vmlinuz-linux-lts', '/mnt/boot/syslinux/syslinux.cfg'))
+  stream_cmd(sed('initramfs-linux', 'initramfs-linux-lts', '/mnt/boot/syslinux/syslinux.cfg'))
+  stream_cmd(sed('Arch Linux', 'Hobby Linux', '/mnt/boot/syslinux/syslinux.cfg'))
+  stream_cmd(sed('Arch Linux', 'Hobby Linux', '/etc/os-release'))
+  stream_cmd(sed('Arch Linux', 'Hobby Linux', '/usr/lib/os-release'))
+  stream_cmd(sed('# Misc option', 'ILoveCandy', '/mnt/etc/pacman.conf'))
+  stream_cmd('echo hobbylinux > /etc/hostname')
+  stream_cmd("cp -rf /etc/systemd/network/* /mnt/etc/systemd/network/")
   arch_chroot_runner(activate_service("systemd-resolved systemd-networkd"))
 end
