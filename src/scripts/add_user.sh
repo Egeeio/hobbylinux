a86609ab8e4d85ad45196600e1de469e48143a5c
egee-irl <brian@egee.io>
2024-04-08 13:18:10 -0700

    update usr shell path

--- src/scripts/add_user.sh
+++ src/scripts/add_user.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 USER_NAME=hobby
 USER_PASSWORD=hobby
-useradd -m -s /bin/fish ${USER_NAME}
+useradd -m -s /usr/bin/fish ${USER_NAME}
 echo -e "${USER_PASSWORD}\n${USER_PASSWORD}" | passwd ${USER_NAME}
 usermod -aG adm ${USER_NAME}
 echo "${USER_NAME} ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/${USER_NAME}"
