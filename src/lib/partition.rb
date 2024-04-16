35bbb1b3bd7ae9b9f501de82e1fae76d7b90b500
egee-irl <brian@egee.io>
2024-04-15 19:46:58 -0700

    make it simplier

--- src/lib/partition.rb
+++ src/lib/partition.rb
@@ -1,19 +1,6 @@
+# The \n's make the ``'s difficult to convert to stream_cmd
 def partition(disk)
-  create_partition_table(disk)
-  create_partition(disk)
-  create_file_system(disk)
-end
-
-private
-
-def create_partition_table(disk)
-  `echo -e "o\nw\n" | fdisk "/dev/#{disk}"`
-end
-
-def create_partition(disk)
-  `echo -e "n\np\n1\n\n\nw\n" | fdisk "/dev/#{disk}"`
-end
-
-def create_file_system(disk)
-  `mkfs.btrfs "/dev/#{disk}1"`
+  puts `echo -e "o\nw\n" | fdisk "/dev/#{disk}"`
+  puts `echo -e "n\np\n1\n\n\nw\n" | fdisk "/dev/#{disk}"`
+  puts `mkfs.btrfs "/dev/#{disk}1"`
 end
