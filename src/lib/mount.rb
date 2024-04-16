fe32965a07f0493efa4346b6ef3bc29f4cfff8c0
egee-irl <brian@egee.io>
2024-04-15 19:18:46 -0700

    simplify mount

--- src/lib/mount.rb
+++ src/lib/mount.rb
@@ -1,6 +1,3 @@
-def mount(mount_point='/mnt', partition='/dev/sda1')
-  unless Dir.exist?(mount_point)
-    Dir.mkdir(mount_point)
-  end
-  `mount #{partition} #{mount_point}`
+def mount(disk)
+  `mount /dev/#{disk}1 /mnt`
 end
