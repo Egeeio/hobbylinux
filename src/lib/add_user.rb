199185fd43bdb495145d141d9c9a871726acb331
egee-irl <brian@egee.io>
2024-04-17 17:44:33 -0700

    Refactor to generate password

--- src/lib/add_user.rb
+++ src/lib/add_user.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
-
+require 'securerandom'
 require_relative 'helpers'
 
 def add_user(user_name='hobby')
+  passwd = SecureRandom.hex[-10..-1]
   stream_cmd('cp -f scripts/add_user.sh /mnt/root/add_user.sh')
-  arch_chroot_runner("/root/add_user.sh #{user_name}")
+  arch_chroot_runner("/root/add_user.sh #{user_name} #{passwd}")
+  passwd
 end
