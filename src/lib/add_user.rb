f83adcb77d256f1ec28fb37d60865a22fb4cc7c3
egee-irl <brian@egee.io>
2024-04-18 11:55:25 -0700

    run outside the chroot

--- src/lib/add_user.rb
+++ src/lib/add_user.rb
@@ -4,7 +4,7 @@ require_relative 'helpers'
 
 def add_user(user_name='hobby')
   passwd = SecureRandom.hex[-10..-1]
-  stream_cmd('cp -f scripts/add_user.sh /mnt/root/add_user.sh')
+  puts `cp -f scripts/add_user.sh /mnt/root/add_user.sh`
   arch_chroot_runner("/root/add_user.sh #{user_name} #{passwd}")
   passwd
 end
