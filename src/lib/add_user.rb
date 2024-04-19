8c195d3c8a731dc2a2bea42e7484bd72359561c2
egee-irl <brian@egee.io>
2024-04-19 12:53:54 -0700

    shorter temporary password

--- src/lib/add_user.rb
+++ src/lib/add_user.rb
@@ -3,7 +3,7 @@ require 'securerandom'
 require_relative 'helpers'
 
 def add_user(user_name='hobby')
-  passwd = SecureRandom.hex[-10..-1]
+  passwd = SecureRandom.hex[-5..-1]
   puts `cp -f scripts/add_user.sh /mnt/root/add_user.sh`
   arch_chroot_runner("/root/add_user.sh #{user_name} #{passwd}")
   passwd
