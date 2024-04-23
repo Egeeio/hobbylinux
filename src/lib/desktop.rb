c1a8b29e2cf0b167a583c6ddd6b6c429ec7b7e03
egee-irl <brian@egee.io>
2024-04-23 14:02:43 -0700

    break into functions

--- src/lib/desktop.rb
+++ src/lib/desktop.rb
@@ -4,9 +4,36 @@ require 'fileutils'
 require_relative 'helpers'
 
 def desktop(user)
-  pacman_install('htop lightdm lightdm-slick-greeter mate mate-extra otf-montserrat deepin-icon-theme arc-gtk-theme pipewire')
+  install_coreutils
+  install_lightdm
+  install_desktop
+  configure_desktop(user)
+end
+
+def install_coreutils
+  pacman_install('htop ruby git curl wget')
+end
+
+def install_lightdm
+  pacman_install('lightdm lightdm-slick-greeter')
+  sed('#greeter-session=example-gtk-gnome', 'greeter-session=lightdm-slick-greeter', '/etc/lightdm/lightdm.conf')
   activate_service('lightdm')
-  # Desktop
+end
+
+def install_desktop
+  pacman_install('mate mate-extra otf-montserrat deepin-icon-theme arc-gtk-theme pipewire')
+end
+
+def configure_desktop(user)
+  # Folders
+  ['.local/bin', 'Documents', 'Downloads', 'Music', 'Pictures', 'Projects', 'Public', 'Videos'].each do |folder|
+    stream_cmd("mkdir -p /home/#{user}/#{folder}")
+  end
+  stream_cmd("ln -s /home/#{user}/.local/bin /home/#{user}/Projects/bin")
+  # Panel
+  FileUtils.mv('files/hobbylinux.layout', '/mnt/usr/share/mate-panel/layouts')
+  arch_chroot_runner("gsettings set org.mate.panel.general default-layout 'hobbylinux'")
+  # Interface
   arch_chroot_runner("gsettings set org.mate.desktop.interface document-font-name 'Montserrat Medium 10'")
   arch_chroot_runner("gsettings set org.mate.desktop.interface font-name 'Montserrat Medium 10'")
   arch_chroot_runner("gsettings set org.mate.desktop.interface gtk-theme 'Arc'")
@@ -17,12 +44,4 @@ def desktop(user)
   arch_chroot_runner("gsettings set org.mate.marco.general theme 'Arc'")
   # Caja
   arch_chroot_runner("gsettings set org.mate.caja.desktop font 'Montserrat Medium 10'")
-  ['.local/bin', 'Documents', 'Downloads', 'Music', 'Pictures', 'Projects', 'Public', 'Videos'].each do |folder|
-    stream_cmd("mkdir -p /home/#{user}/#{folder}")
-  end
-  stream_cmd("ln -s /home/#{user}/.local/bin /home/#{user}/Projects/bin")
-
-  # Panel
-  FileUtils.mv('files/hobbylinux.layout', '/mnt/usr/share/mate-panel/layouts')
-  arch_chroot_runner("gsettings set org.mate.panel.general default-layout 'hobbylinux'")
 end
