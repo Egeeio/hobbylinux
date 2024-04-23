24f1118475426d604cd215b84c1eb8e6937a989a
egee-irl <brian@egee.io>
2024-04-23 12:02:51 -0700

    add folders, fonts, and update panel

--- src/lib/desktop.rb
+++ src/lib/desktop.rb
@@ -1,19 +1,28 @@
 # frozen_string_literal: true
 
+require 'FileUtils'
 require_relative 'helpers'
 
-def desktop
-  pacman_install('htop lightdm lightdm-slick-greeter mate mate-extra otf-montserrat deepin-icon-theme arc-gtk-theme')
+def desktop(user)
+  pacman_install('htop lightdm lightdm-slick-greeter mate mate-extra otf-montserrat deepin-icon-theme arc-gtk-theme pipewire')
   activate_service('lightdm')
-# Desktop
+  # Desktop
   arch_chroot_runner("gsettings set org.mate.desktop.interface document-font-name 'Montserrat Medium 10'")
   arch_chroot_runner("gsettings set org.mate.desktop.interface font-name 'Montserrat Medium 10'")
   arch_chroot_runner("gsettings set org.mate.desktop.interface gtk-theme 'Arc'")
   arch_chroot_runner("gsettings set org.mate.desktop.interface icon-name 'vintage'")
   arch_chroot_runner("gsettings set org.mate.desktop.interface monospace-font-name 'Hack Bold 10'")
-# Marco
+  # Marco
   arch_chroot_runner("gsettings set org.mate.marco.general document-font-name 'Montserrat Medium 10'")
   arch_chroot_runner("gsettings set org.mate.marco.general theme 'Arc'")
-# Caja
+  # Caja
   arch_chroot_runner("gsettings set org.mate.caja.desktop font 'Montserrat Medium 10'")
+  ['.local/bin', 'Documents', 'Downloads', 'Music', 'Pictures', 'Projects', 'Public', 'Videos'].each do |folder|
+    stream_cmd("mkdir -p /home/#{user}/#{folder}")
+  end
+  stream_cmd("ln -s /home/#{user}/.local/bin /home/#{user}/Projects/bin")
+
+  # Panel
+  FileUtils.mv('files/hobbylinux.layout', '/mnt/usr/share/mate-panel/layouts')
+  arch_chroot_runner("gsettings set org.mate.panel.general default-layout 'hobbylinux'")
 end
