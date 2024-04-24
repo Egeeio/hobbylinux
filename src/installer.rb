44bec93549fd51c3f7cd010aeeec93c50e491b7e
egee-irl <brian@egee.io>
2024-04-24 13:08:47 -0700

    clean up banner

--- src/installer.rb
+++ src/installer.rb
@@ -9,21 +9,24 @@ require_relative 'lib/add_user'
 require 'optparse'
 
 OptionParser.new do |opts|
-  disk = 'sda'
-  user = 'hobby'
+  opts.banner = "Welcome to the Hobby Linux Installer! 🚀\nUsage: ./installer.rb -i [DISK],[USERNAME]"
+  opts.on('-h', '--help', 'Show this help message') do
+    puts opts
+    exit
+  end
   opts.on('-i DISK USER', Array, 'Install Hobby Linux on DISK with USER') do |args|
     disk = args[0]
     user = args[1]
   end
+  disk ||= 'sda'
+  user ||= 'hobby'
+  puts "Installing to #{sda} with user #{user}."
+  puts "Install will begin in 5 seconds..."
+  sleep 5
   partition(disk)
   mount(disk)
   bootstrap
   desktop(user)
   passwd = add_user(user)
   puts "Installation Complete.\nReboot into Hobby Linux and log in as:\nUsername: #{user}\nOne-time password: #{passwd}"
-
-  opts.on('-h', '--help', 'Show this help message') do
-    puts opts
-  end
-  opts.banner = "Welcome to the Hobby Linux Installer! 🚀\nUsage: ./installer.rb [DISK],[USER]"
 end.parse!
