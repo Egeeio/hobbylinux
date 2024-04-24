e0436438b785e1f6678fdc588771f9d60c6699cd
egee-irl <brian@egee.io>
2024-04-24 13:01:08 -0700

    rearrange params so as to not require -i every time

--- src/installer.rb
+++ src/installer.rb
@@ -9,19 +9,21 @@ require_relative 'lib/add_user'
 require 'optparse'
 
 OptionParser.new do |opts|
-  opts.banner = "Welcome to the Hobby Linux Installer! 🚀\nUsage: ./installer.rb [DISK],[USER]"
+  disk = 'sda'
+  user = 'hobby'
   opts.on('-i DISK USER', Array, 'Install Hobby Linux on DISK with USER') do |args|
     disk = args[0]
-    user = args[1] || 'hobby'
-    partition(disk)
-    mount(disk)
-    bootstrap
-    desktop(user)
-    passwd = add_user(user)
-    puts "Installation Complete.\nReboot into Hobby Linux and log in as:\nUsername: #{user}\nOne-time password: #{passwd}"
+    user = args[1]
   end
+  partition(disk)
+  mount(disk)
+  bootstrap
+  desktop(user)
+  passwd = add_user(user)
+  puts "Installation Complete.\nReboot into Hobby Linux and log in as:\nUsername: #{user}\nOne-time password: #{passwd}"
 
   opts.on('-h', '--help', 'Show this help message') do
     puts opts
   end
+  opts.banner = "Welcome to the Hobby Linux Installer! 🚀\nUsage: ./installer.rb [DISK],[USER]"
 end.parse!
