f505d6affca7dffead52baf9099c776bd3ad15c0
egee-irl <brian@egee.io>
2024-09-14 09:18:21 -0700

    switch to cinnamon

--- src/lib/desktop.rb
+++ src/lib/desktop.rb
@@ -3,6 +3,6 @@
 require_relative 'helpers'
 
 def desktop
-  pacman_install('htop lightdm lightdm-gtk-greeter mate mate-extra')
+  pacman_install('htop lightdm lightdm-gtk-greeter gtk-theme-elementary elementary-icon-theme cinnamon')
   activate_service('lightdm')
 end
