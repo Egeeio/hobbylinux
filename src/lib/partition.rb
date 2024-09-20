6d1ed0ddf41a684dfff4a228e66501f9346d1fb7
egee-irl <brian@egee.io>
2024-09-20 06:01:26 -0700

    Force partition creation

--- src/lib/partition.rb
+++ src/lib/partition.rb
@@ -3,6 +3,6 @@
 def partition(disk)
   puts `echo -e "o\nw\n" | fdisk "/dev/#{disk}"`
   puts `echo -e "n\np\n1\n\n\nw\n" | fdisk "/dev/#{disk}"`
-  # puts `mkfs.xfs "/dev/#{disk}1"`
-  puts `mkfs.btrfs "/dev/#{disk}1"`
+  # puts `mkfs.xfs -f "/dev/#{disk}1"`
+  puts `mkfs.btrfs -f "/dev/#{disk}1"`
 end
