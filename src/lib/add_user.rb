d91e2dff5020935bd6d97bf8545f9ee834764251
egee-irl <brian@egee.io>
2024-10-06 08:55:18 -0700

    hardcode password for now

--- src/lib/add_user.rb
+++ src/lib/add_user.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 require 'securerandom'
 require_relative 'helpers'
 
-def add_user(user_name='hobby')
-  passwd = SecureRandom.hex[-5..-1]
+def add_user(user_name = 'hobby')
+  passwd = user_name # SecureRandom.hex[-5..]
   puts `cp -f scripts/add_user.sh /mnt/root/add_user.sh`
   arch_chroot_runner("/root/add_user.sh #{user_name} #{passwd}")
   passwd
