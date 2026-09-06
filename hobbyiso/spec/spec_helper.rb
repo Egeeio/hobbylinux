088fb2389231ae8759fd6469e68852d0ebb02dcd
egeexyz <brian@egee.io>
2026-09-06 14:53:09 -0700

    add test spec helper

---
+++ hobbyiso/spec/spec_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
+end
